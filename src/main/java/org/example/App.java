0a37ac19fc4458e7e027b15d6d541715764b36fe
whilefor1010 <whilefor1010@gmail.com>
2022-11-15 19:49:48 +0200

    add ArrUtils (sort and search) v2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -9,7 +11,21 @@ public class App
     public static void main( String[] args )
     {
 
+        int[] arr = new int[]{1,-8,15,22,31,10,10,0};
 
+        System.out.println(Arrays.toString(arr));
+
+        ArrUtils arrUtils = new ArrUtils();
+
+        arrUtils.sortBubble(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+        int goal = -8;
+        System.out.println("###################### Searching... ######################");
+        System.out.println(goal);
+
+        System.out.println(arrUtils.mrBinSearch(arr,goal));
 
     }
 }
--- src/main/java/org/example/ArrUtils.java
+++ src/main/java/org/example/ArrUtils.java
@@ -1,7 +1,83 @@
 package org.example;
 
-public class SortUtils {
+import java.util.Arrays;
 
-    
+public class ArrUtils {
+
+    public int[] swap(int[] arr, int i, int j){
+
+        int tmp = arr[i];
+
+        arr[i] = arr[j];
+        arr[j] = tmp;
+
+        System.out.println(Arrays.toString(arr));
+
+        return arr;
+
+    }
+
+    public int[] sortBubble(int[] arr){
+
+        for (int i = arr.length; i >=0 ; i--) {
+
+            for (int j = 0; j < i-1; j++) {
+
+                if (arr[j] > arr[j+1]) {
+                    swap(arr, j, j+1);
+                }
+
+            }
+            
+        }
+
+        return arr;
+
+    }
+
+    public int mrBinSearch(int[] arr, int goal){
+
+        int index = -1;
+
+        arr = sortBubble(arr);
+
+        int i = 0;
+        int j = arr.length;
+
+        while (true){
+
+            System.out.println(Arrays.toString(arr));
+
+            int tmpIndex = (j+i)/2;
+
+            if(arr[tmpIndex]==goal) {
+                index = tmpIndex;
+                break;
+            }
+
+            if(tmpIndex == 0 || tmpIndex == arr.length){
+                break;
+            }
+
+            if(arr[tmpIndex]<goal){
+                i = tmpIndex;
+            } else {
+                j = tmpIndex;
+            }
+
+            //Обнулить часть массива для красоты
+            for (int k = 0; k < i; k++) {
+                arr[k] = 0;
+            }
+
+            for (int k = j+1; k < arr.length; k++) {
+                arr[k] = 0;
+            }
+
+        }
+
+        return index;
+
+    }
 
 }
